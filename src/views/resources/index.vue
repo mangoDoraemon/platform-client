5550060340a13127785f64c669a34270aa6805c2
chenlong53 <13587734356@139.com>
2021-01-13 18:20:56 +0800

    新增考核指标报表、资源上报页面

--- src/views/resources/index.vue
+++ src/views/resources/index.vue
@@ -1,10 +1,33 @@
 <template>
-    <h1>资源上报</h1>
+  <div class="app-container">
+    <el-form :inline="true">
+      <el-form-item label="选择日期:"><el-date-picker v-model="date_picker"></el-date-picker></el-form-item>
+      <el-form-item label="对照批次号:"><el-input></el-input></el-form-item>
+      <el-button type="primary">查找</el-button>
+    </el-form>
+
+    <el-table :data="tableData">
+      <el-table-column label="对照批次号"></el-table-column>
+      <el-table-column label="对照稽查名称"></el-table-column>
+      <el-table-column label="参照结果"></el-table-column>
+      <el-table-column label="对照结果"></el-table-column>
+      <el-table-column label="浙江省杭州市三墩机房(pod1)"></el-table-column>
+      <el-table-column label="浙江省杭州市石桥机房(pod2)"></el-table-column>
+      <el-table-column label="浙江省杭州市石桥机房(pod3)"></el-table-column>
+      <el-table-column label="浙江省金华市金华机房(pod4)"></el-table-column>
+      <el-table-column label="浙江POD5（池外）"></el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'index'
+    name: 'index',
+    data(){
+      return{
+        date_picker:''
+      }
+    }
   }
 </script>
 
