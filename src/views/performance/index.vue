572841fc24f243f345f3f56ccbe8cf1801eb3f2b
luolx <1619754646@qq.com>
2021-01-14 11:38:49 +0800

    [添加]添加性能上报，工单上报，告警报表页面

--- src/views/performance/index.vue
+++ src/views/performance/index.vue
@@ -1,15 +1,101 @@
 <template>
-<h1>
-  性能上报
-</h1>
+  <div class="app-container">
+
+  <el-row :gutter="10" class="mb8">
+    <el-col :span="1.5">
+      <span style="font-family: 微软雅黑;font-size: large;font-weight: 800" >性能上报</span>
+    </el-col>
+  </el-row>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="选择日期：">
+        <el-date-picker
+          v-model="datatime"
+          type="date"
+          placeholder="选择日期" size="small">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="对照批次号：">
+        <el-input v-model="datatime" placeholder="请输入对照批次号" clearable size="small" style="width: 240px"  />
+      </el-form-item>
+      <el-form-item>
+      <el-button type="cyan" icon="el-icon-search" size="mini" @click="onSubmit">查找</el-button>
+    </el-form-item>
+    </el-form>
+    <el-table
+    :data="tableData"
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="date"
+      label="对照批次号"
+      width="100"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="模型名称"
+      width="100"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="province"
+      label="浙江省杭州市三墩机房(pod1)参考/对照"
+      width="200"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="city"
+      label="浙江省杭州市三墩机房(pod2)参考/对照"
+      width="200"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="浙江省杭州市三墩机房(pod3)参考/对照"
+      width="200"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="zip"
+      label="浙江省杭州市三墩机房(pod4)参考/对照"
+      width="200"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="zip"
+      label="浙江POSS(池外)参考/对照"
+      width="120"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="zip"
+      label="总计参考/对照"
+      width="100"
+      align="center">
+    </el-table-column>
+  </el-table>
+</div>
 </template>
 
 <script>
   export default {
-    name: 'index'
+    name: 'index',
+    data(){
+      return{
+        datatime:'',
+        formInline: {
+          user: '',
+          region: ''
+        },
+        tableData:[]
+      }
+
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
   }
 </script>
 
-<style scoped>
-
-</style>
--- src/views/repairOrder/index.vue
+++ src/views/repairOrder/index.vue
@@ -1,10 +1,195 @@
 <template>
-    <h1>工单报表</h1>
+  <div class="app-container">
+    <el-form :inline="true"  class="demo-form-inline">
+      <el-form-item label="选择日期：">
+        <el-date-picker
+          v-model="datatime"
+          type="date"
+          placeholder="选择日期" size="small">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="cyan" icon="el-icon-search" size="mini" @click="onSubmit">查找</el-button>
+      </el-form-item>
+    </el-form>
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <span style="font-family: 微软雅黑;font-size: large;font-weight: 500;line-height: 50px" >工单报表</span>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      :span-method="objectSpanMethod"
+      show-summary
+    >
+      <el-table-column
+        prop="id"
+        label="工单类型"
+        width="160"
+        align="center"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="操作类型"
+        width="130"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="amount1"
+        label="工单总量"
+        width="130"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="amount2"
+        label="成功数"
+        width="130"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="amount3"
+        label="失败数"
+        width="130"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="amount3"
+        label="未上报"
+        width="130"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="amount3"
+        label="失败原因"
+        width="200"
+        align="center">
+      </el-table-column>
+
+    </el-table>
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <span style="font-family: 微软雅黑;font-size: large;font-weight: 500;line-height: 50px" >工单处理质量</span>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+
+    >
+      <el-table-column
+        prop="id"
+        label="工单类型"
+        width="340"
+        align="center"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="工单数量"
+        width="340"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="amount1"
+        label="及时率"
+        width="340"
+        align="center">
+      </el-table-column>
+
+
+
+    </el-table>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'index'
+    name: 'index',
+    data(){
+      return{
+        tableData: [{
+          id: '12987122',
+          name: '王小虎',
+          amount1: '234',
+          amount2: '3.2',
+          amount3: 10
+        }, {
+          id: '12987123',
+          name: '王小虎',
+          amount1: '165',
+          amount2: '4.43',
+          amount3: 12
+        }, {
+          id: '12987124',
+          name: '王小虎',
+          amount1: '324',
+          amount2: '1.9',
+          amount3: 9
+        }, {
+          id: '12987125',
+          name: '王小虎',
+          amount1: '621',
+          amount2: '2.2',
+          amount3: 17
+        }, {
+          id: '12987126',
+          name: '王小虎',
+          amount1: '539',
+          amount2: '4.1',
+          amount3: 15
+        }],
+        datatime:''
+      }
+    },
+    methods:{
+      objectSpanMethod({ row, column, rowIndex, columnIndex }) {
+        if (columnIndex === 0) {
+          if (rowIndex % 2 === 0) {
+            return {
+              rowspan: 2,
+              colspan: 1
+            };
+          } else {
+            return {
+              rowspan: 0,
+              colspan: 0
+            };
+          }
+        }
+      },
+      getSummaries(param) {
+        const { columns, data } = param;
+        const sums = [];
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            sums[index] = '总价';
+            return;
+          }
+          const values = data.map(item => Number(item[column.property]));
+          if (!values.every(value => isNaN(value))) {
+            sums[index] = values.reduce((prev, curr) => {
+              const value = Number(curr);
+              if (!isNaN(value)) {
+                return prev + curr;
+              } else {
+                return prev;
+              }
+            }, 0);
+            sums[index] += ' 元';
+          } else {
+            sums[index] = 'N/A';
+          }
+        });
+
+        return sums;
+      },
+      onSubmit() {
+        console.log('submit!');
+      }
+
+    }
   }
 </script>
 
