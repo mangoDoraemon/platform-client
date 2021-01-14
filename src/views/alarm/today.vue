2790a713ef5c4ca412934da596d611fd1d5f2054
luolx <1619754646@qq.com>
2021-01-14 20:39:57 +0800

    [修改]修改性能上报

---
+++ src/views/alarm/today.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="app-container">
+  <el-form :inline="true" :model="formInline" class="demo-form-inline" >
+    <el-form-item label="选择日期：" >
+      <el-date-picker
+        v-model="datatime"
+        type="date"
+        placeholder="选择日期" size="small">
+      </el-date-picker>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="cyan" icon="el-icon-search" size="mini" @click="onSubmit">查找</el-button>
+    </el-form-item>
+  </el-form>
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+
+  >
+    <el-table-column
+      prop="date"
+      label=""
+      width="130"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="集团上报情况"
+      width="130"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="总批次"
+      width="130"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="上报成功批次"
+      width="130"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="上报失败批次"
+      width="130"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="未上报批次"
+      width="130"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="上报失败/未上报原因"
+      width="200"
+      align="center">
+    </el-table-column>
+  </el-table>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "today",
+      data(){
+          return{
+            datatime:'',
+            formInline: {
+              user: '',
+              region: ''
+            },
+            tableData:[],
+          }
+      },
+      methods: {
+        onSubmit() {
+          console.log('submit!');
+        },
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/performance/index.vue
+++ src/views/performance/index.vue
@@ -6,80 +6,25 @@
       <span style="font-family: 微软雅黑;font-size: large;font-weight: 800" >性能上报</span>
     </el-col>
   </el-row>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="选择日期：">
-        <el-date-picker
-          v-model="datatime"
-          type="date"
-          placeholder="选择日期" size="small">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="对照批次号：">
-        <el-input v-model="datatime" placeholder="请输入对照批次号" clearable size="small" style="width: 240px"  />
-      </el-form-item>
-      <el-form-item>
-      <el-button type="cyan" icon="el-icon-search" size="mini" @click="onSubmit">查找</el-button>
-    </el-form-item>
-    </el-form>
-    <el-table
-    :data="tableData"
-    style="width: 100%"
-  >
-    <el-table-column
-      prop="date"
-      label="对照批次号"
-      width="100"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="模型名称"
-      width="100"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="province"
-      label="浙江省杭州市三墩机房(pod1)参考/对照"
-      width="200"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="city"
-      label="浙江省杭州市三墩机房(pod2)参考/对照"
-      width="200"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="浙江省杭州市三墩机房(pod3)参考/对照"
-      width="200"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="zip"
-      label="浙江省杭州市三墩机房(pod4)参考/对照"
-      width="200"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="zip"
-      label="浙江POSS(池外)参考/对照"
-      width="120"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="zip"
-      label="总计参考/对照"
-      width="100"
-      align="center">
-    </el-table-column>
-  </el-table>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="今日概览" name="first" :key="'first'">
+        <today v-if="first"/>
+      </el-tab-pane>
+      <el-tab-pane label="批次详情" name="second" :key="'second'">
+
+        <detail v-if="second"/>
+      </el-tab-pane>
+    </el-tabs>
+
 </div>
 </template>
 
 <script>
+  import detail  from "../alarm/detail";
+  import today from "../alarm/today";
   export default {
     name: 'index',
+    components: {detail,today},
     data(){
       return{
         datatime:'',
@@ -87,13 +32,26 @@
           user: '',
           region: ''
         },
-        tableData:[]
-      }
+        tableData:[],
+        first:true,
+        second:false,
+        activeName:'first',
 
+      }
     },
     methods: {
       onSubmit() {
         console.log('submit!');
+      },
+      handleClick(tab, event) {
+        this.activeName = tab.name;
+        if(tab.name=='first'){
+          this.first=true;
+          this.second=false;
+        }else if(tab.name=='second'){
+          this.first=false;
+          this.second=true;
+        }
       }
     }
   }
