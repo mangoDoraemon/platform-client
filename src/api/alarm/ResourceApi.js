7d7d42d78d8d748a6685c8e8b5fff95351e3a3d0
chenlong53 <13587734356@139.com>
2021-03-02 09:56:33 +0800

    新增EXCEL导出

--- src/api/alarm/ResourceApi.js
+++ src/api/alarm/ResourceApi.js
@@ -7,3 +7,37 @@ export function getList(query) {
     params: query
   })
 }
+
+export function exportExcel() {
+  return request({
+    url:'/export/export',
+    method:'get',
+  })
+}
+
+export function download() {
+  return request({
+    url:'/export/download',
+    method:'get'
+  })
+}
+
+export function exportExcel2() {
+  return request({
+    url:'/export/export-role',
+    method:'get',
+  })
+}
+
+// 下载文件
+export function downloadFile(obj, name, suffix) {
+  const url = window.URL.createObjectURL(new Blob([obj]))
+  const link = document.createElement('a')
+  link.style.display = 'none'
+  link.href = url
+  const fileName = Date.parse(new Date()) + '-' + name + '.' + suffix
+  link.setAttribute('download', fileName)
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
